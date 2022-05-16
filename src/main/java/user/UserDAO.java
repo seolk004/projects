f7b6856925717390ff37cafd539ecf3ec9b23f2e
karavan458 <karavan458@gmail.com>
2022-05-16 16:37:33 +0900

    add login module

--- src/main/java/user/UserDAO.java
+++ src/main/java/user/UserDAO.java
@@ -45,4 +45,20 @@ public class UserDAO {
         }
         return -2; //오류
     }
+
+    public int join(User user) {
+        String sql = "insert into user values(?, ?, ?, ?, ?)";
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, user.getUserID());
+            pstmt.setString(2, user.getUserPassword());
+            pstmt.setString(3, user.getUserName());
+            pstmt.setString(4, user.getUserGender());
+            pstmt.setString(5, user.getUserEmail());
+            return pstmt.executeUpdate();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
 }
